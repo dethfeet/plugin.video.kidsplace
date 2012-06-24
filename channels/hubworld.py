a07735beb18c1a7e4337a39dc3772892e50502cc
dethfeet <dethfeet@qip.ru>
2012-06-24 14:26:04 +0200

    hubworld.py: add plot to videos

--- channels/hubworld.py
+++ channels/hubworld.py
@@ -15,11 +15,11 @@ const = "d833dffff3160dc3ca9269e875abd8805b3b0f38"
 playerID = 1379595107001
 publisherID = 90719631001
 
-extractSection = re.compile("<section class=\".*?video-box\">.*?<div class=\"fb-top\">.*?<h2>(.*?)</h2>(.*?)<div class=\"fb-bottom\">",re.DOTALL)
+extractSection = re.compile("<section class=\".*?video-box\">.*?<div class=\"fb-top\">.*?<h2>(.*?)</h2>(.*?)<div class=\"fb-bottom\">", re.DOTALL)
 
 def mainPage():
-    helper.addDirectoryItem("Videos", {"channel":thisChannel,"action":"showVideos","link":baseLink+"videos"}, "")
-    helper.addDirectoryItem("Shows", {"channel":thisChannel,"action":"showShows","link":baseLink+"shows"}, "")
+    helper.addDirectoryItem("Videos", {"channel":thisChannel, "action":"showVideos", "link":baseLink + "videos"}, "")
+    helper.addDirectoryItem("Shows", {"channel":thisChannel, "action":"showShows", "link":baseLink + "shows"}, "")
     helper.endOfDirectory()
     
 def showVideos(link):
@@ -28,7 +28,7 @@ def showVideos(link):
     extractShowMoreButton = re.compile("<a href=\"(.*?)\" class=\"lite-button\">")
     extractSections = extractSection.findall(page)
     if len(extractSections) == 1:
-        showVideosSection(link,extractSections[0][0])
+        showVideosSection(link, extractSections[0][0])
         return False
     else:
         for section in extractSections:
@@ -38,54 +38,55 @@ def showVideos(link):
             showMoreButton = extractShowMoreButton.search(sectionHtml)
             sectionLinkSection = sectionTitle
             if showMoreButton is not None:
-                sectionLink = baseLink+showMoreButton.group(1)
+                sectionLink = baseLink + showMoreButton.group(1)
                 sectionLinkSection = ""
-            helper.addDirectoryItem(sectionTitle, {"channel":thisChannel,"action":"showVideosSection","link":sectionLink,"section":sectionLinkSection}, "")
+            helper.addDirectoryItem(sectionTitle, {"channel":thisChannel, "action":"showVideosSection", "link":sectionLink, "section":sectionLinkSection}, "")
     
     helper.endOfDirectory()
     
-def showVideosSection(link,section):
-	link = urllib.unquote(link)
-	showSection = urllib.unquote_plus(section)
-	
-	page = helper.load_page(link)
-	
-	extractVideo = re.compile("<section class=\".*?\">.*?<img src=\"(.*?)\".*?<a href=\"(.*?)\" class=\"title\">(.*?)</a>.*?<p>(.*?)</p>.*?</section>",re.DOTALL)
+def showVideosSection(link, section):
+    link = urllib.unquote(link)
+    showSection = urllib.unquote_plus(section)
+    
+    page = helper.load_page(link)
+    
+    extractVideo = re.compile("<section class=\".*?\">.*?<img src=\"(.*?)\".*?<a href=\"(.*?)\" class=\"title\">(.*?)</a>.*?<p>(.*?)</p>.*?<span class=\"content-item-short-description\">(.*?)</span>.*?</section>", re.DOTALL)
 
-	sectionHtml = "";
-	
-	if showSection != "":
-		extractSections = extractSection.finditer(page)
-		for section in extractSections:
-			sectionTitle = section.group(1)
-			if sectionTitle == showSection:
-				sectionHtml = section.group(2)
-	else:
-		sectionHtml = page
-	
-	for video in extractVideo.finditer(sectionHtml):
-		videoTitle = video.group(3) + " (" + video.group(4).strip() + ")"
-		videoImg = baseLink+video.group(1)
-		videoLink = baseLink+video.group(2)
-		helper.addDirectoryItem(videoTitle, {"channel":thisChannel,"action":"playVideo","link":videoLink}, videoImg, False)
+    sectionHtml = "";
+    
+    if showSection != "":
+        extractSections = extractSection.finditer(page)
+        for section in extractSections:
+            sectionTitle = section.group(1)
+            if sectionTitle == showSection:
+                sectionHtml = section.group(2)
+    else:
+        sectionHtml = page
 
-	helper.endOfDirectory()
+    for video in extractVideo.finditer(sectionHtml):
+        videoTitle = video.group(3) + " (" + video.group(4).strip() + ")"
+        videoImg = baseLink + video.group(1)
+        videoLink = baseLink + video.group(2)
+        videoPlot = video.group(5)
+        helper.addDirectoryItem(videoTitle, {"channel":thisChannel, "action":"playVideo", "link":videoLink}, videoImg, False,plot=videoPlot)
+
+    helper.endOfDirectory()
 
 def showShows(link):
-	page = helper.load_page(urllib.unquote(link))
-	
-	extractShow = re.compile("<section class=\".*?content-item-vertical.*?\">.*?<div class=\"thumbimg\">.*?src=\"(.*?)\".*?href=\"(.*?)\".*?>(.*?)</a>",re.DOTALL)
-	
-	for show in extractShow.finditer(page):
-		showImg = baseLink+show.group(1)
-		showLink = baseLink+show.group(2)+"/videos"
-		showTitle = show.group(3)
-		
-		showPage = urllib.urlopen(showLink)
-		if showLink == showPage.geturl():
-			if extractSection.search(showPage.read()) is not None:
-				helper.addDirectoryItem(showTitle, {"channel":thisChannel,"action":"showVideos","link":showLink}, showImg)
-	helper.endOfDirectory()
+    page = helper.load_page(urllib.unquote(link))
+    
+    extractShow = re.compile("<section class=\".*?content-item-vertical.*?\">.*?<div class=\"thumbimg\">.*?src=\"(.*?)\".*?href=\"(.*?)\".*?>(.*?)</a>", re.DOTALL)
+    
+    for show in extractShow.finditer(page):
+        showImg = baseLink + show.group(1)
+        showLink = baseLink + show.group(2) + "/videos"
+        showTitle = show.group(3)
+        
+        showPage = urllib.urlopen(showLink)
+        if showLink == showPage.geturl():
+            if extractSection.search(showPage.read()) is not None:
+                helper.addDirectoryItem(showTitle, {"channel":thisChannel, "action":"showVideos", "link":showLink}, showImg)
+    helper.endOfDirectory()
 
 def playVideo(link):
     page = helper.load_page(urllib.unquote(link))
@@ -105,7 +106,7 @@ else:
     if params['action'] == "showVideos":
         showVideos(params['link'])
     if params['action'] == "showVideosSection":
-        showVideosSection(params['link'],params['section'])
+        showVideosSection(params['link'], params['section'])
     if params['action'] == "showShows":
         showShows(params['link'])
     if params['action'] == "playVideo":
